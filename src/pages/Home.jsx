8cdff3ebaa06261525d867df6c7f9984d11cde89
Keval <keval9464@gmail.com>
2023-09-15 18:19:10 +0530

    added-github

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer"
+import MainCon from "../components/MainCon"
+import NavBar from "../components/NavBar"
+
+const Home = () => {
+    return (
+        <>
+            <NavBar />
+            <MainCon />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
