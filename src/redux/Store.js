3a947d4097f46cf17cd2dcbdb1ad8ba76e3ccecb
Keval <keval9464@gmail.com>
2023-09-20 20:54:04 +0530

    error-solving

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit"
 import CartSlice from "./slice/CartSlice";
-import categorySlice from "./slice/categorySlice";
+import categorySlice from "./slice/CategorySlice";
 import SearchSlice from "./slice/SearchSlice";
 
 const Store = configureStore({
